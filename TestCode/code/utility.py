2e79c0e56591a14b321f3c7aa05ebbd8cfed043a
Lucy Zhao <lucyzhao.zlx@gmail.com>
2021-04-13 22:04:27 -0400

    SSIM calculation

--- TestCode/code/utility.py
+++ TestCode/code/utility.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import scipy.misc as misc
+from scipy import signal
+import skimage.color as sc
 
 import torch
 import torch.optim as optim
@@ -184,7 +186,80 @@ def calc_psnr(sr, hr, scale, rgb_range, benchmark=False):
     valid = diff[:, :, shave:-shave, shave:-shave]
     mse = valid.pow(2).mean()
 
-    return -10 * math.log10(mse)
+    return - 10 * math.log10(mse)
+
+"""
+Source https://github.com/HolmesShuan/EDSR-ssim
+"""   
+def matlab_style_gauss2D(shape=(3,3),sigma=0.5):
+  """
+  2D gaussian mask - should give the same result as MATLAB's fspecial('gaussian',[shape],[sigma])
+  Acknowledgement : https://stackoverflow.com/questions/17190649/how-to-obtain-a-gaussian-filter-in-python (Author@ali_m)
+  """
+  m,n = [(ss-1.)/2. for ss in shape]
+  y,x = np.ogrid[-m:m+1,-n:n+1]
+  h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+  h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+  sumh = h.sum()
+  if sumh != 0:
+    h /= sumh
+  return h
+
+"""
+Source https://github.com/HolmesShuan/EDSR-ssim
+""" 
+def calc_ssim(X, Y, scale, rgb_range, dataset=None, sigma=1.5, K1=0.01, K2=0.03, R=255):
+  '''
+  X : y channel (i.e., luminance) of transformed YCbCr space of X
+  Y : y channel (i.e., luminance) of transformed YCbCr space of Y
+  Please follow the setting of psnr_ssim.m in EDSR (Enhanced Deep Residual Networks for Single Image Super-Resolution CVPRW2017).
+  Official Link : https://github.com/LimBee/NTIRE2017/tree/db34606c2844e89317aac8728a2de562ef1f8aba
+  The authors of EDSR use MATLAB's ssim as the evaluation tool, 
+  thus this function is the same as ssim.m in MATLAB with C(3) == C(2)/2. 
+  '''
+  gaussian_filter = matlab_style_gauss2D((11, 11), sigma)
+
+  if dataset and dataset.dataset.benchmark:
+    shave = scale
+    if X.size(1) > 1:
+        gray_coeffs = [65.738, 129.057, 25.064]
+        convert = X.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
+        X = X.mul(convert).sum(dim=1)
+        Y = Y.mul(convert).sum(dim=1)
+  else:
+    shave = scale + 6
+
+  X = X[..., shave:-shave, shave:-shave].squeeze().cpu().numpy().astype(np.float64) 
+  Y = Y[..., shave:-shave, shave:-shave].squeeze().cpu().numpy().astype(np.float64)
+  
+  # Assume args.n_colors = 3
+  X = np.moveaxis(X, 0, -1)
+  Y = np.moveaxis(Y, 0, -1)
+  X = sc.rgb2ycbcr(X)[:, :, 0]
+  Y = sc.rgb2ycbcr(Y)[:, :, 0]
+
+  window = gaussian_filter
+
+  ux = signal.convolve2d(X, window, mode='same', boundary='symm')
+  uy = signal.convolve2d(Y, window, mode='same', boundary='symm')
+
+  uxx = signal.convolve2d(X*X, window, mode='same', boundary='symm')
+  uyy = signal.convolve2d(Y*Y, window, mode='same', boundary='symm')
+  uxy = signal.convolve2d(X*Y, window, mode='same', boundary='symm')
+
+  vx = uxx - ux * ux
+  vy = uyy - uy * uy
+  vxy = uxy - ux * uy
+
+  C1 = (K1 * R) ** 2
+  C2 = (K2 * R) ** 2
+
+  A1, A2, B1, B2 = ((2 * ux * uy + C1, 2 * vxy + C2, ux ** 2 + uy ** 2 + C1, vx + vy + C2))
+  D = B1 * B2
+  S = (A1 * A2) / D
+  mssim = S.mean()
+
+  return mssim
 
 def make_optimizer(args, my_model):
     trainable = filter(lambda x: x.requires_grad, my_model.parameters())
